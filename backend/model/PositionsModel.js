9a1675cb21d91791c646b10baa881e81ca4f174f
Anupam0010 <165364891+Anupam0010@users.noreply.github.com>
2026-01-21 21:19:33 +0530

    Add files via upload

---
+++ backend/model/PositionsModel.js
@@ -0,0 +1,8 @@
+const mongoose = require("mongoose");
+const { PositionsSchema } = require("../schemas/PositionsSchema");
+
+const PositionsModel =
+  mongoose.models.Position ||
+  mongoose.model("Position", PositionsSchema);
+
+module.exports = PositionsModel;
